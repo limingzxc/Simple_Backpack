c1480f85c360a4e21ea4d135071f4d5e853f161f
limingzxc <2904962732@qq.com>
2023-01-25 22:25:06 +0800

    update

--- src/main/java/com/wwwday/simple_backpack/init/ModContainers.java
+++ src/main/java/com/wwwday/simple_backpack/init/ModContainers.java
@@ -5,9 +5,7 @@ import com.wwwday.simple_backpack.container.BackpackContainer;
 import com.wwwday.simple_backpack.inventory.BackpackInventory;
 import com.wwwday.simple_backpack.items.Backpack;
 import net.minecraft.inventory.container.ContainerType;
-import net.minecraft.item.Item;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.extensions.IForgeContainerType;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
@@ -22,10 +20,10 @@ public class ModContainers {
             = CONTAINERS.register("big_backpack_container",
             () -> IForgeContainerType.create(((windowId, inv, data) -> {
                 boolean isMainHand = inv.player.getMainHandItem().getItem() == ModItems.BIG_BACKPACK.get();
-                Item item = isMainHand? inv.getSelected().getItem(): inv.player.getOffhandItem().getItem();
-                Backpack backpack = item instanceof Backpack? (Backpack) item: null;
+                ItemStack stack = isMainHand? inv.player.getMainHandItem(): inv.player.getOffhandItem();
+                Backpack backpack = stack.getItem() instanceof Backpack? (Backpack) stack.getItem(): null;
                 BackpackInventory inventory =
-                        new BackpackInventory(inv.getSelected(), inv.player, backpack != null? backpack.getRows(): 0);
+                        new BackpackInventory(stack, inv.player, backpack != null? backpack.getRows(): 0);
                 return BackpackContainer.sixRows(windowId, inv, inventory.getItemStackHandler(), isMainHand);
             })));
 
@@ -33,10 +31,10 @@ public class ModContainers {
             = CONTAINERS.register("small_backpack_container",
             () -> IForgeContainerType.create(((windowId, inv, data) -> {
                 boolean isMainHand = inv.player.getMainHandItem().getItem() == ModItems.SMALL_BACKPACK.get();
-                Item item = isMainHand? inv.getSelected().getItem(): inv.player.getOffhandItem().getItem();
-                Backpack backpack = item instanceof Backpack? (Backpack) item: null;
+                ItemStack stack = isMainHand? inv.player.getMainHandItem(): inv.player.getOffhandItem();
+                Backpack backpack = stack.getItem() instanceof Backpack? (Backpack) stack.getItem(): null;
                 BackpackInventory inventory =
-                        new BackpackInventory(inv.getSelected(), inv.player, backpack != null? backpack.getRows(): 0);
+                        new BackpackInventory(stack, inv.player, backpack != null? backpack.getRows(): 0);
                 return BackpackContainer.threeRows(windowId, inv, inventory.getItemStackHandler(), isMainHand);
             })));
 
